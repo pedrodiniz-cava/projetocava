174009062a3a3c1015817b281d3f4b436eb24aae
Mateus Santos <mateuscorrea560@gmail>
2026-04-22 14:10:58 -0300

    Interface projetocava

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,23 @@
-//
+document.addEventListener("DOMContentLoaded", () => {
+
+    const form = document.getElementById("loginForm");
+
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        if(email === "" || password === "") {
+            alert("Preencha todos os campos!");
+            return;
+        }
+
+        console.log("Email:", email);
+        console.log("Senha:", password);
+
+        // Aqui você pode integrar com Laravel (axios ou fetch)
+        alert("Login enviado!");
+    });
+
+});
