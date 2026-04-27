239535ed7f3ecb4a8ff0118a8d1ffafd8d124a02
Pdrooks <pedrodpiress73@gmail.com>
2026-04-27 15:47:16 -0300

    qualidade logo

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,21 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("loginForm");
 
     form.addEventListener("submit", function(e) {
-        e.preventDefault();
-
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
 
         if(email === "" || password === "") {
+            e.preventDefault();
             alert("Preencha todos os campos!");
-            return;
         }
-
-        console.log("Email:", email);
-        console.log("Senha:", password);
-
-        // Aqui você pode integrar com Laravel (axios ou fetch)
-        alert("Login enviado!");
     });
 
 });
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,30 +16,35 @@
         <div class="form-section">
             <div class="form-content">
                 <div class="logo">
-                    <img src="{{ asset('images/logo.png') }}" alt="Logo">
+                    <img src="{{ asset('images/primecarterlogo.png') }}" alt="Logo">
                 </div>
 
                 <h2>Bem-Vindo!</h2>
                 <p class="subtitle">Faça login de acesso para sua conta</p>
 
-                <form id="loginForm">
+                @error('email')
+                    <p style="color:red; margin-bottom:10px;">{{ $message }}</p>
+                @enderror
+
+                <form id="loginForm" method="POST" action="/login">
+                    @csrf
                     <div class="input-group">
                         <label for="email">Endereço de Email</label>
                         <div class="input-wrapper">
-                            <input type="email" id="email" placeholder="seu@gmail.com" required>
+                            <input type="email" id="email" name="email" placeholder="seu@gmail.com" required>
                         </div>
                     </div>
 
                     <div class="input-group">
                         <label for="password">Senha</label>
                         <div class="input-wrapper">
-                            <input type="password" id="password" placeholder="........" required>
+                            <input type="password" id="password" name="password" placeholder="........" required>
                         </div>
                     </div>
 
                     <div class="options">
                         <label class="checkbox-container">
-                            <input type="checkbox" id="remember">
+                            <input type="checkbox" id="remember" name="remember">
                             <span class="checkmark"></span>
                             Lembrar-me por 30 dias
                         </label>
